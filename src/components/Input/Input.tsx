fc6eb50d036f0f8ebfb27bc72d5dab0012a21ddb
Manoela Cassia <manoelacs@ic.ufal.br>
2021-12-10 14:15:25 -0300

    Removendo itens desnecessários

--- src/components/Input/Input.tsx
+++
@@ -1,9 +0,0 @@
-import { InputProps } from 'antd';
-import { FC } from 'react';
-import * as S from './styles';
-
-const Input:FC< InputProps > = ( props ) => {
-    return(
-        <S.StyledInput {...props} />
-    )
- }; export default Input;
